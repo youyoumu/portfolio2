66e1177277e3aa87482b8aa94283aabfc7162b64
youyoumu <19760884+youyoumu@users.noreply.github.com>
2025-07-14 19:54:00 +0700

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+import solidPlugin from 'vite-plugin-solid'
+import tailwindcss from '@tailwindcss/vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    TanStackRouterVite({ target: 'solid', autoCodeSplitting: true }),
+    solidPlugin(),
+    tailwindcss(),
+  ],
+})
