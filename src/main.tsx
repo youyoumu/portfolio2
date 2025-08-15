555ac2bef273019016bf3d409ed8a6da4dc04b0c
youyoumu <19760884+youyoumu@users.noreply.github.com>
2025-08-15 21:55:16 +0700

    Add smooth scroller

--- src/main.tsx
+++ src/main.tsx
@@ -22,14 +22,17 @@ declare module "@tanstack/solid-router" {
 }
 
 function App() {
-  return (
-    <>
-      <RouterProvider router={router} />
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 const rootElement = document.getElementById("app");
 if (rootElement) {
   render(() => <App />, rootElement);
 }
+
+ScrollSmoother.create({
+  content: rootElement,
+  smooth: 1, // how long (in seconds) it takes to "catch up" to the native scroll position
+  effects: true, // looks for data-speed and data-lag attributes on elements
+  smoothTouch: 0.1, // much shorter smoothing time on touch devices (default is NO smoothing on touch devices)
+});
--- src/routes/-components/RootPage.tsx
+++ src/routes/-components/RootPage.tsx
@@ -1,6 +1,8 @@
 import { createSignal, Show } from "solid-js";
+import { Portal } from "solid-js/web";
 
 import { env } from "#/env";
+import { hidePortalDiv } from "#/lib/utils/hidePortalDiv";
 
 import { createBackground } from "./createBackground";
 import { Curtain } from "./Curtain";
@@ -12,12 +14,17 @@ export default function RootPage() {
 
   return (
     <div class="relative">
-      <div class="fixed z-[-10] top-0 left-0 overflow-hidden h-svh w-full">
-        {background.gameOfLife.canvas}
-        <div class="absolute inset-0 flex items-center justify-center pointer-events-none">
-          <div class="p-8">{background.lyrics.container}</div>
+      <Portal
+        mount={document.getElementById("background") ?? undefined}
+        ref={hidePortalDiv}
+      >
+        <div class="fixed z-[-10] top-0 left-0 overflow-hidden h-svh w-full">
+          {background.gameOfLife.canvas}
+          <div class="absolute inset-0 flex items-center justify-center pointer-events-none">
+            <div class="p-8">{background.lyrics.container}</div>
+          </div>
         </div>
-      </div>
+      </Portal>
       {/* <Show when={env.DEV}> */}
       {/*   <div class="fixed top-0 left-0 flex gap-1 flex-wrap p-1 "> */}
       {/*     <DebugPanel background={background} /> */}
@@ -25,7 +32,12 @@ export default function RootPage() {
       {/* </Show> */}
       <div class="h-svh w-full"></div>
       <div class="h-svh w-full">test</div>
-      {background.audioControl}
+      <Portal
+        mount={document.getElementById("audio-control") ?? undefined}
+        ref={hidePortalDiv}
+      >
+        {background.audioControl}
+      </Portal>
       <Show when={!hide()}>
         <div class="absolute top-0 left-0 overflow-hidden h-svh w-full">
           <Curtain onHide={() => setHide(true)} />
