47211e619b69acf12c6aa1766f24e14e95bf5368
youyoumu <19760884+youyoumu@users.noreply.github.com>
2025-08-18 21:41:36 +0700

    Add snapping to section

--- src/routes/-components/Content.tsx
+++ src/routes/-components/Content.tsx
@@ -1,8 +1,30 @@
+import { onMount } from "solid-js";
+
 export function Content() {
+  let section1!: HTMLDivElement;
+  let section2!: HTMLDivElement;
+  let section3!: HTMLDivElement;
+
+  onMount(() => {
+    const sections = [section1, section2, section3];
+    gsap.to(sections, {
+      scrollTrigger: {
+        snap: {
+          snapTo: 1 / (sections.length - 1),
+          duration: 1,
+        },
+        scrub: false,
+      },
+    });
+  });
+
   return (
     <>
-      <div class="h-svh w-full"></div>
-      <div class="h-svh w-full">test</div>
+      <div ref={section1} class="h-svh w-full">
+        test{" "}
+      </div>
+      <div ref={section2} class="h-svh w-full bg-blue-500/50"></div>
+      <div ref={section3} class="h-svh w-full bg-red-500/50"></div>
     </>
   );
 }
