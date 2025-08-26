1680f23630ea77b85fd22b40c246ed1b350e2f95
youyoumu <19760884+youyoumu@users.noreply.github.com>
2025-08-26 20:35:35 +0700

    Arrange the icons

--- src/routes/-components/Content.tsx
+++ src/routes/-components/Content.tsx
@@ -15,6 +15,44 @@ export function Content() {
     .getPropertyValue("--color-neutral-content")
     .trim();
 
+  const iconClass = "size-16 sm:size-20";
+
+  const icons = [
+    <TypescriptIcon
+      class={iconClass}
+      path1Props={{
+        fill: iconColor,
+      }}
+    />,
+    <DockerIcon
+      class={iconClass}
+      path1Props={{
+        fill: iconColor,
+      }}
+    />,
+    <NixIcon
+      class={iconClass}
+      path1Props={{
+        fill: iconColor,
+      }}
+      path2Props={{
+        fill: iconColor,
+      }}
+    />,
+    <ReactIcon
+      class={iconClass}
+      g1Props={{
+        fill: iconColor,
+      }}
+    />,
+    <NeovimIcon
+      class={iconClass}
+      path1Props={{
+        fill: iconColor,
+      }}
+    />,
+  ];
+
   onMount(() => {
     const sections = [section1, section2, section3];
     gsap.to(sections, {
@@ -34,40 +72,23 @@ export function Content() {
       <div ref={section1} class="h-svh w-full">
         test{" "}
       </div>
-      <div ref={section2} class="h-svh w-full bg-black/10">
-        <TypescriptIcon
-          class="size-16"
-          path1Props={{
-            fill: iconColor,
-          }}
-        />
-        <DockerIcon
-          class="size-16"
-          path1Props={{
-            fill: iconColor,
-          }}
-        />
-        <NixIcon
-          class="size-16"
-          path1Props={{
-            fill: iconColor,
-          }}
-          path2Props={{
-            fill: iconColor,
-          }}
-        />
-        <ReactIcon
-          class="size-16"
-          g1Props={{
-            fill: iconColor,
-          }}
-        />
-        <NeovimIcon
-          class="size-16"
-          path1Props={{
-            fill: iconColor,
-          }}
-        />
+      <div
+        ref={section2}
+        class="h-svh w-full bg-black/10 text-neutral-content flex flex-col items-center justify-center "
+      >
+        <div class="flex flex-wrap gap-1 max-w-52 sm:max-w-64">
+          {icons[0]}
+          {icons[1]}
+          {icons[2]}
+          {icons[3]}
+          {icons[4]}
+          <div class="size-20 overflow-visible leading-none">
+            <div class="text-lg text-nowrap">youyoumu</div>
+            <div class="text-nowrap">WEB DEVELOPER</div>
+            <div class="text-nowrap">LINUX ENTHUSIAST</div>
+            <div class="text-nowrap opacity-40">WEEB</div>
+          </div>
+        </div>
       </div>
       <div ref={section3} class="h-svh w-full bg-black/20"></div>
     </>
