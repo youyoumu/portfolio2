7e1cb3f68f3d5b38abe31fd24dddb90b18d767b7
youyoumu <19760884+youyoumu@users.noreply.github.com>
2025-08-29 17:25:45 +0700

    feat: add aochi

--- src/routes/-components/Content/Section5.tsx
+++ src/routes/-components/Content/Section5.tsx
@@ -1,5 +1,16 @@
 export function Section5() {
   return (
-    <div class="h-svh w-full bg-black/20 flex flex-col justify-center items-center"></div>
+    <div class="h-svh w-full bg-black/20 flex flex-col justify-center items-center">
+      <div class="h-[50svh] rounded-sm overflow-hidden">
+        <video
+          src="/video/aochi2.mp4"
+          class="w-full h-full object-cover"
+          muted
+          autoplay
+          loop
+        ></video>
+      </div>
+      <p class="text-sm text-neutral-content/50">hire me pls</p>
+    </div>
   );
 }
