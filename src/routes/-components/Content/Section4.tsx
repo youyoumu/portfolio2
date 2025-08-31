e7d26c4cd6e4d77289697593751df8ad76b3d8ae
youyoumu <19760884+youyoumu@users.noreply.github.com>
2025-08-31 13:21:21 +0700

    feat: add marker content

--- src/routes/-components/Content/Section4.tsx
+++ src/routes/-components/Content/Section4.tsx
@@ -1,21 +1,64 @@
-import { createSignal } from "solid-js";
+import { type Component, createSignal, type JSX } from "solid-js";
 
 import { cn } from "#/lib/utils/cn";
 
 type Marker = {
   x: number; // % position
   y: number;
-  text: string;
+  text: JSX.Element;
 };
 
 const markers: Marker[] = [
   {
-    x: 30,
-    y: 40,
-    text: "This is my keyboard, custom built with lubed switches.",
+    x: 54,
+    y: 47,
+    text: "I use Neovim as my primary code edior. I started using it since January 2025.",
   },
-  { x: 60, y: 70, text: "My monitor setup, perfect for coding + anime." },
-  { x: 80, y: 20, text: "A small figure from my favorite series." },
+  {
+    x: 52,
+    y: 68,
+    text: "Pressplay APOLLO61 Lite, 60% Keyboard. I use this for gaming and opt out from my split keyboard.",
+  },
+  {
+    x: 44,
+    y: 78,
+    text: "A custom split keyboard with corne layout. I switch to this not long ago, and I'm getting used to it.",
+  },
+  {
+    x: 84,
+    y: 81,
+    text: "Vertical mouse because using normal mouse for long period of time hurts my hand. Rexus Cliff.",
+  },
+  {
+    x: 71,
+    y: 74,
+    text: "Logitect G102",
+  },
+  {
+    x: 19,
+    y: 77,
+    text: "I do some gaming. Some games that I play in the recent years are: Minecraft, Factorio, Counter Strike 2, Zenless Zone Zero. Although currently I only use this controller for ZZZ and reviewing my Anki cards. Logitech F310.",
+  },
+  {
+    x: 83,
+    y: 33,
+    text: "This is my main PC, I dual boot NixOS and Windows. I mainly use linux for everything that can be done in linux, and Windows for some games.",
+  },
+  {
+    x: 82,
+    y: 51,
+    text: "I have a home server. This is my MiniPC, I ssh-ed into it. I use it to host some of my projects, game server (Minecraft and Factorio), and some apps with Docker.",
+  },
+  {
+    x: 21,
+    y: 28,
+    text: "Steelseries Arctis 5, since 2019.",
+  },
+  {
+    x: 93,
+    y: 62,
+    text: "This is Anki, a flashcard program that I use to help me learn Japanese language. I started learning since November 2023, and my current level is probably around JLPT N3. I plan to get JLPT N2 test next year.",
+  },
 ];
 
 export function Section4() {
@@ -24,6 +67,7 @@ export function Section4() {
   const [y, setY] = createSignal(50);
   const [zoom, setZoom] = createSignal(1);
   const [hoveredMarker, setHoveredMarker] = createSignal<number | null>(null);
+  const [showMarker, setShowMarker] = createSignal(false);
 
   function handleMouseMove(e: MouseEvent) {
     if (!containerRef) return;
@@ -59,6 +103,7 @@ export function Section4() {
     }
     rafId = requestAnimationFrame(animate);
     setZoom(1);
+    setShowMarker(false);
   }
 
   return (
@@ -75,6 +120,7 @@ export function Section4() {
           "hover:[&>div]:scale-150": zoom() === 1,
           "hover:[&>div]:scale-250": zoom() === 2,
         })}
+        onMouseEnter={() => setShowMarker(true)}
         onMouseMove={handleMouseMove}
         onMouseLeave={handleMouseLeave}
         onClick={() => {
@@ -96,12 +142,17 @@ export function Section4() {
         <div
           class={cn(
             "transition-transform duration-700 ease-in-out absolute top-0 left-0 size-full",
+            {
+              "opacity-0": !showMarker(),
+            },
           )}
           style={{ "transform-origin": `${x()}% ${y()}%` }}
         >
           {markers.map((m, i) => (
             <div
-              class="absolute"
+              class={cn("absolute", {
+                "z-10": hoveredMarker() === i,
+              })}
               style={{
                 top: `${m.y}%`,
                 left: `${m.x}%`,
@@ -110,11 +161,18 @@ export function Section4() {
               onMouseEnter={() => setHoveredMarker(i)}
               onMouseLeave={() => setHoveredMarker(null)}
             >
-              {/* dot */}
-              <div class="w-4 h-4 rounded-full bg-warning border-2 border-base-100 shadow-md hover:scale-125 transition-transform cursor-pointer"></div>
+              <div class="w-4 h-4 rounded-full bg-warning border-2 border-base-100 shadow-md hover:scale-125 transition-transform"></div>
 
               {hoveredMarker() === i && (
-                <div class="absolute bottom-full left-1/2 -translate-x-1/2 mb-2 border-1 bg-neutral text-neutral-content text-sm px-2 py-1 rounded-lg shadow-lg whitespace-nowrap">
+                <div
+                  class={cn(
+                    "scale-50 origin-bottom transition-transform duration-700 absolute bottom-full left-1/2 -translate-x-1/2 mb-2 border-1 bg-neutral text-neutral-content text-sm px-2 py-1 rounded-lg shadow-lg w-64",
+                    {
+                      "-translate-x-6/10": m.x > 90,
+                      "-translate-x-8/12": m.x > 90 && zoom() === 2,
+                    },
+                  )}
+                >
                   {m.text}
                 </div>
               )}
