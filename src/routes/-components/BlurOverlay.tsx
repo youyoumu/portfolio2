8e9ee8bd362459e717f0d1e97ff44b1f82692fb6
youyoumu <19760884+youyoumu@users.noreply.github.com>
2025-08-31 14:46:12 +0700

    feat: crt background

--- src/routes/-components/BlurOverlay.tsx
+++ src/routes/-components/BlurOverlay.tsx
@@ -3,6 +3,7 @@ import { onMount } from "solid-js";
 export function BlurOverlay() {
   let overlay!: HTMLDivElement;
   let overlay2!: HTMLDivElement;
+  let overlay3!: HTMLDivElement;
 
   onMount(() => {
     gsap.to(overlay, {
@@ -26,6 +27,17 @@ export function BlurOverlay() {
         scrub: true,
       },
     });
+
+    gsap.to(overlay3, {
+      opacity: 0.5,
+      ease: "none",
+      scrollTrigger: {
+        trigger: overlay,
+        start: "top top",
+        end: "bottom top",
+        scrub: true,
+      },
+    });
   });
 
   return (
@@ -34,6 +46,10 @@ export function BlurOverlay() {
       <div
         ref={overlay2}
         class="h-svh w-full absolute top-0 left-0 bg-black opacity-0"
+      ></div>
+      <div
+        ref={overlay3}
+        class="bg-crt h-svh w-full absolute top-0 left-0 opacity-0"
       ></div>
     </>
   );
