763e751d319495db6d70c1d3b36e41178a063ab3
youyoumu <19760884+youyoumu@users.noreply.github.com>
2025-09-02 15:38:03 +0700

    feat: add text to SideNav

--- src/routes/-components/SideNav.tsx
+++ src/routes/-components/SideNav.tsx
@@ -1,13 +1,23 @@
-import { createSignal, onMount } from "solid-js";
+import { createEffect, createSignal, onMount } from "solid-js";
 
 import { cn } from "#/lib/utils/cn";
 
 gsap.registerPlugin(ScrollTrigger);
 
+const headings: string[] = [
+  "GAME OF LIFE",
+  "ESSENCE",
+  "WORKS",
+  "ENVIRONMENT",
+  "CONTACT",
+];
+
 export function SideNav(props: { sections: HTMLDivElement[] | undefined }) {
   const [activeIndex, setActiveIndex] = createSignal(0);
   const [hoveredIndex, setHoveredIndex] = createSignal<number | null>(null);
+  const headingRefs: HTMLDivElement[] = [];
 
+  const headingTweens: gsap.core.Tween[] = [];
   onMount(() => {
     if (!props.sections) return;
 
@@ -20,12 +30,47 @@ export function SideNav(props: { sections: HTMLDivElement[] | undefined }) {
         onEnterBack: () => setActiveIndex(i),
       });
     });
+
+    headingRefs.forEach((ref, i) => {
+      SplitText.create(ref, {
+        type: "words,lines",
+        autoSplit: true,
+        mask: "lines",
+        onSplit: (self) => {
+          headingTweens[i] = gsap.fromTo(
+            self.lines,
+            {
+              yPercent: 100,
+            },
+            {
+              duration: 1,
+              yPercent: 0,
+              ease: "expo.out",
+              paused: true,
+            },
+          );
+        },
+      });
+    });
+  });
+
+  createEffect(() => {
+    const index = hoveredIndex();
+    headingTweens.forEach((tween, i) => {
+      if (i === index) {
+        headingTweens[index]?.duration(1);
+        headingTweens[index]?.play();
+        return;
+      }
+      tween.duration(0.2);
+      tween.reverse();
+    });
   });
 
   return (
     <div
       class={cn(
-        "fixed right-8 top-1/2 flex flex-col gap-4 -translate-y-1/2 duration-200 items-center w-9",
+        "fixed right-8 top-1/2 flex flex-col gap-4 -translate-y-1/2 duration-200 items-center",
         {
           "opacity-0": activeIndex() === 0,
         },
@@ -33,7 +78,7 @@ export function SideNav(props: { sections: HTMLDivElement[] | undefined }) {
     >
       {props.sections?.map((_, i) => (
         <button
-          class={cn("h-3 duration-300 ", {
+          class={cn("h-3 duration-300 relative", {
             "py-3": activeIndex() === i,
             "cursor-pointer": activeIndex() !== 0,
           })}
@@ -47,9 +92,14 @@ export function SideNav(props: { sections: HTMLDivElement[] | undefined }) {
           <div
             class={cn("h-[1px] w-8 bg-neutral-content duration-100", {
               "h-1": activeIndex() === i || hoveredIndex() === i,
-              "w-9": hoveredIndex() === i,
             })}
           ></div>
+          <div
+            ref={headingRefs[i]}
+            class="hidden xl:block absolute top-1/2 right-0 -translate-x-12 -translate-y-1/2 text-sm text-neutral-content text-nowrap leading-none pointer-events-none"
+          >
+            {headings[i]}
+          </div>
         </button>
       ))}
     </div>
