da4342c46ae71f4d672e9ef10a69a1e598c57056
youyoumu <19760884+youyoumu@users.noreply.github.com>
2026-04-29 17:44:08 +0700

    refactor: move components dir

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,8 +1,7 @@
+import RootPage from "#/components/RootPage";
 import { createFileRoute } from "@tanstack/solid-router";
 import { zodValidator } from "@tanstack/zod-adapter";
 import { z } from "zod";
-
-import RootPage from "./-components/RootPage";
 
 export const Route = createFileRoute("/")({
   validateSearch: zodValidator(
